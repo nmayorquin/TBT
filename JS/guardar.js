2c7bd887dbe0550773b8d6092e60b21a764d86cd
naty <natymayorn@gmail.com>
2022-09-06 20:27:36 -0500

    vista crud administrador ya sirve y ya tiene sus links de imagenes, he tenido que modificar el index para funcionamiento con firebase y eliminé el txt johe porque ya no nos sirve

--- JS/guardar.js
+++ JS/guardar.js
@@ -1,14 +1,36 @@
+
+//VALIDACIÓN DE INGRESO Y CONTRASEÑA DE USUARIO AL SISTEMA
+
+const auth = firebase.auth(); //con este comando le digo a firebase que me traiga unos datos para autenticación
+    const signupForm = document.getElementById("formularioInscripcion");
+
+    signupForm.addEventListener("submit",(eventoReinicio) => {
+       eventoReinicio.preventDefault();//por defecto los formularios reinician la página, pero con esta línea se bloquea ese reinicio
+       
+        const email= document.getElementById("email_usuario").value;
+        const password= document.getElementById("pass_usuario").value;
+        
+        auth
+        .createUserWithEmailAndPassword(email, password)
+
+        .then(userCredential => {
+
+            console.log("en autenticación");
+        })
+    
+    });
+
 //función para formulario de index
 
 function guardar (){
     db.collection("usuarios").add({
-        nombres:document.getElementById("nombre_usuario").value,
-        apellidos: document.getElementById("apellido_usuario").value,
+        nombres:document.getElementById("nombre-apellido_usuario").value,
+        //email: document.getElementById("email_usuario").value,
         identificacion: document.getElementById("id_usuario").value,
-        password: document.getElementById("pass_usuario").value,
+        //password: document.getElementById("pass_usuario").value,
         repetPass: document.getElementById("repetaPass_usuario").value,
-        rolAdmi: document.getElementById("admin").value,
-        rolEmp: document.getElementById("empl").value,
+        /*rolAdmi: document.getElementById("admin").value,
+        rolEmp: document.getElementById("empl").value,*/
     })
     .then((docRef) => {
         alert("Su registro fue exitoso");
@@ -16,24 +38,7 @@ function guardar (){
     .catch((error) => {
         alert("error en el registro");
     });
-
 }
 
-//VALIDACIÓN DE INGRESO Y CONTRASEÑA DE USUARIO AL SISTEMA
-/*const auth = getAuth();
-createUserWithEmailAndPassword(auth, identificacion, password)
-.then((userCredential) => {
-    // Signed in
-    const user = userCredential.user;
-    // ...
-})
-.catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorCode);
-    console.log(errorCode);
-
-    // ..
-});*/
 
 
