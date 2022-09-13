39fe8d01753beb655ad7d39be6b829d9ce8161b0
naty <natymayorn@gmail.com>
2022-09-13 17:01:52 -0500

    haciendo archivos separados para autenticación

--- JS/autenticacion.js
+++ JS/autenticacion.js
@@ -1,4 +1,4 @@
-//VALIDACIÓN DE INGRESO Y CONTRASEÑA DE USUARIO AL SISTEMA
+//CAPTURA DE DATOS PARA LOGIN
 
     const auth = firebase.auth(); //con este comando le digo a firebase que me traiga unos datos para autenticación
     const signupForm = document.getElementById("formularioInscripcion");
@@ -14,8 +14,19 @@
 
         .then(userCredential => {
 
-            console.log("en autenticación");
+            console.log("en autenticación"); //si este mensaje se muestra en consola, significa que se ejecutó el método
         })
-    
-    
     });
+
+
+    //VALIDACIÓN DE INGRESO Y CONTRASEÑA DE USUARIO AL SISTEMA
+
+    var formulario_ingreso = document.getElementById("ingreso-plataforma");
+
+    formulario_ingreso.addEventListener("a", e => {
+
+        const email= document.getElementById("ingreso-plataforma__email").value;
+        const password= document.getElementById("ingreso-plataforma__pass").value;
+        console.log("email, password");
+
+    })
